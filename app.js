4c028ae7db2b7c830c05c1fcc0d625769a482719
gonrodter <gonrodter@alum.us.es>
2021-08-09 14:05:24 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+
+tl.to('.text', {y:'0%', duration: 1, stagger: 0.25});
+tl.to('.slider', {y:'-100%', duration: 1.5, delay: 0.5});
+tl.to('.intro', {y: '-100%', duration: 1}, '-=1');
+tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, '-=1');
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    const bigText = document.querySelector('.big-text');
+    
+    burger.addEventListener('click', () => {
+        //Toggle nav
+        nav.classList.toggle('nav-active');
+
+        //Animate Links
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+                bigText.style.animation = '';
+            }else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+                bigText.style.animation = `bigTextFade 0.2s ease forwards 0.2s`;
+            }
+        });
+        //Burger animation
+        burger.classList.toggle('toggle');
+
+    });
+    
+
+}
+
+navSlide();
+
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".cup", 8, { y: -300 })
+  .to(".pencil", 8, { x: -250 }, "-=8")
+  .to(".notebook", 8, { y: -375 }, "-=8")
+  .to(".big-text", 8, { x: 375 }, "-=8")
+  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 8 }, "-=8")
+  .to(".projects-content", 10, { top: "0%" }, "-=8")
+  .fromTo(".content-cards", { opacity: 0 }, { opacity: 1, duration: 1.5 })
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "240%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
+
+
+function contactMeVisible() {
+  var T = document.getElementById("contactme");
+  T.style.display = "flex";  // <-- Set it to block
+}
---
+++ parallax.js
@@ -0,0 +1,20 @@
+let controller = new ScrollMagic.Controller();
+let timeline = new TimelineMax();
+
+timeline
+  .to(".cup", 10, { y: -300 })
+  .to(".pencil", 10, { y: -200 }, "-=10")
+  .to(".notebook", 10, { y: -200 }, "-=10")
+  .fromTo(".bg1", { y: -50 }, { y: 0, duration: 10 }, "-=10")
+  .to(".content", 10, { top: "0%" }, "-=10")
+  .fromTo(".content-images", { opacity: 0 }, { opacity: 1, duration: 3 })
+  .fromTo(".text", { opacity: 0 }, { opacity: 1, duration: 3 });
+
+let scene = new ScrollMagic.Scene({
+  triggerElement: "section",
+  duration: "300%",
+  triggerHook: 0,
+})
+  .setTween(timeline)
+  .setPin("section")
+  .addTo(controller);
