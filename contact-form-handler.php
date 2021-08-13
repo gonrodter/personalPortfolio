fa8cbc84915d1d7b7e040ae6f76fa38a6bf574d0
gonrodter <gonrodter@alum.us.es>
2021-08-13 12:56:41 +0200

    contactform changes

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -16,9 +16,9 @@
 
     $to = "rodriguez.terron.gonzalo@gmail.com";
 
-    $headers .= "From: $email_from \r\n";
+    $headers = "From: $email_from \r\n";
 
-    $headers = "Reply-To: $email \r\n";
+    $headers .= "Reply-To: $email \r\n";
 
     mail($to, $email_subject, $email_body, $headers);
 
